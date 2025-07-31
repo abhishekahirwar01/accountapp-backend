dcf20ceb4693588ea64a44cbe045082444ad3782
finaxis002 <finaxis.ai@gmail.com>
2025-07-31 17:44:47 +0530

    added master profile api

--- models/MasterAdmin.js
+++ models/MasterAdmin.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 const masterAdminSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
-  password: {type: String, required: true },
+  password: { type: String, required: true },
+  role: { type: String, default: "master" }
 }, { timestamps: true });
 
 module.exports = mongoose.model("MasterAdmin", masterAdminSchema);
