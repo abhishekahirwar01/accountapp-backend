cc16c66ded3f167ca527a230d7166e46d42b0ff4
Finaxis002 <finaxis.ai@gmail.com>
2025-08-01 18:09:34 +0530

    sales and purchase api with party and product api

---
+++ controllers/productController.js
@@ -0,0 +1,21 @@
+const Product = require("../models/Product");
+
+exports.createProduct = async (req, res) => {
+  try {
+    const { name } = req.body;
+
+    const product = new Product({
+      name,
+      createdByClient: req.user.id
+    });
+
+    await product.save();
+    res.status(201).json({ message: "Product created", product });
+
+  } catch (err) {
+    if (err.code === 11000) {
+      return res.status(400).json({ message: "Product already exists for this client" });
+    }
+    res.status(500).json({ message: "Server error", error: err.message });
+  }
+};
--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const connectDB = require("./config/db");
 const masterAdminRoutes = require("./routes/masterAdminRoutes");
 const clientRoutes = require("./routes/clientRoutes");
 const companyRoutes = require("./routes/companyRoutes");
+const salesRoutes = require("./routes/salesEntry");
+const purchaseRoutes = require("./routes/purchaseEntry");
+const productRoutes = require("./routes/productRoute");
+const partyRoutes = require("./routes/partyRoute");
 
 dotenv.config();
 connectDB();
@@ -16,6 +20,10 @@ app.use(express.json());
 app.use("/api/master-admin", masterAdminRoutes);
 app.use("/api/clients", clientRoutes);
 app.use("/api/companies", companyRoutes);
+app.use("/api/sales", salesRoutes);
+app.use("/api/purchase", purchaseRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/parties", partyRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
