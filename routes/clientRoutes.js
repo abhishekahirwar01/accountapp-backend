5fd362333f210cbe28ce221c3f548b73d7fa76ee
Finaxis002 <finaxis.ai@gmail.com>
2025-08-02 19:11:41 +0530

    client user limit

--- routes/clientRoutes.js
+++ routes/clientRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { createClient, getClients, loginClient, updateClient, deleteClient, resetPassword , getClientById} = require("../controllers/clientController");
+const { createClient, getClients, loginClient, updateClient, deleteClient, resetPassword , getClientById, setUserLimit} = require("../controllers/clientController");
 const verifyMasterAdmin = require("../middleware/verifyMasterAdmin");
 
 // Create a client
@@ -21,6 +21,10 @@ router.put("/reset-password/:id", verifyMasterAdmin, resetPassword);
 
 // Get company by ID (client or master)
 router.get("/:id", verifyMasterAdmin, getClientById);
+
+//PUT set user limit
+
+router.put("/:clientId/user-limit",verifyMasterAdmin, setUserLimit)
 
 //client login
 router.post("/login", loginClient);
