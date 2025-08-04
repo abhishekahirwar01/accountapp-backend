1feaf4fb01d823dbbbd74da3b02b6757f0f13845
Finaxis002 <finaxis.ai@gmail.com>
2025-08-04 14:25:31 +0530

    comapny new field

--- models/Company.js
+++ models/Company.js
@@ -1,17 +1,41 @@
 const mongoose = require("mongoose");
 
 const companySchema = new mongoose.Schema({
-  registrationNumber: { type: String, required: true, unique: true },
-  companyName: { type: String, required: true },
-  address: { type: String },
-  companyOwner: { type: String, required: true },
-  contactNumber: { type: String, required: true },
-  gstin: { type: String, default: null },
-  companyType: {
+  businessName: { type: String, required: true },
+  businessType: {
     type: String,
     required: true
   },
-  client: { type: mongoose.Schema.Types.ObjectId, ref: "Client", required: true },
+  address: { type: String },
+  City: { type: String },
+  addressState: { type: String },
+  Country: { type: String },
+  Pincode: { type: String },
+  Telephone: { type: String},
+  mobileNumber: { type: String, required: true },
+  emailId: { type: String, default: null },
+  Website: { type: String, default: null },
+  PANNumber: { type: String, default: null },
+  IncomeTaxLoginPassword: { type: String, default: null },
+  gstin: {type: String, default: null},
+  gstState: {type: String, default: null},
+  RegistrationType: {type: String, default: null},
+  PeriodicityofGSTReturns: {type: String, default: null},
+  GSTUsername: {type: String, default: null},
+  GSTPassword: {type: String, default: null},
+  ewayBillApplicable: {
+    type: String,
+    enum: ["Yes", "No"],
+    default: "No"
+  },
+   EWBBillUsername: {type: String, default: null},
+   EWBBillPassword: {type: String, default: null},
+   TANNumber:{type: String, default: null},
+   TAXDeductionCollectionAcc:{type: String, default: null},
+   DeductorType:{type: String, default: null},
+   TDSLoginUsername:{type: String, default: null},
+   TDSLoginPassword:{type: String, default: null},
+   client: { type: mongoose.Schema.Types.ObjectId, ref: "Client", required: true },
 }, { timestamps: true });
 
 module.exports = mongoose.model("Company", companySchema);
