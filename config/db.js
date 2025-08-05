00ea2dc1885ad87760aea2b6c6b2fb3d44e5e996
Finaxis002 <finaxis.ai@gmail.com>
2025-08-05 12:53:36 +0530

    server vercel

--- config/db.js
+++ config/db.js
@@ -1,13 +1,33 @@
+// const mongoose = require("mongoose");
+// require("dotenv").config();
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGO_URI);
+//     console.log("✅ MongoDB Connected");
+//   } catch (err) {
+//     console.error("❌ DB Connection Error:", err.message);
+//     process.exit(1);
+//   }
+// };
+
+// module.exports = connectDB;
+
+
+
 const mongoose = require("mongoose");
-require("dotenv").config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("✅ MongoDB Connected");
-  } catch (err) {
-    console.error("❌ DB Connection Error:", err.message);
-    process.exit(1);
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`❌ MongoDB Connection Error: ${error.message}`);
+    process.exit(1); // Vercel logs will show exact reason
   }
 };
 
