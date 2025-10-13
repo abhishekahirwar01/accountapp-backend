35eee02462d9a4311ac1aec6a2ae53422d592d5a
finaxis002 <finaxis.ai@gmail.com>
2025-10-13 16:43:52 +0530

    added origin

--- server.js
+++ server.js
@@ -59,7 +59,9 @@ connectDB();
 const allowedOrigins = [
   'https://accountapp-theta.vercel.app',
   'http://localhost:3000',
-  'http://localhost:8678'
+  'http://localhost:8678',
+  "https://vinimay.sharda.co.in",
+  "http://vinimay.sharda.co.in"
 ];
 
 
