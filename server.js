cfadf0ea3a9801d662ca1eeea453cccc6589e94a
Abhishek Ahirwar <ahirwarabhi01@gmail.com>
2025-10-17 15:59:59 +0530

    upload remove

--- server.js
+++ server.js
@@ -89,7 +89,7 @@ app.use(cors({ origin: "*" }));
 // app.use(express.json());
 app.use(express.json({ limit: "15mb" }));
 
-app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
+// app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 
 app.use(async (req, res, next) => {
   console.log(`📨 ${req.method} ${req.url}`);
