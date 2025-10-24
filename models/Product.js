484fce9f0723f640145a2b2f2abd62d78c0aca14
finaxis002 <finaxis.ai@gmail.com>
2025-10-24 12:12:11 +0530

    selling price added in product

--- models/Product.js
+++ models/Product.js
@@ -5,6 +5,7 @@ const productSchema = new mongoose.Schema(
     stocks: { type: Number, default: 0, min: 0 },
     unit: { type: String, trim: true },
     hsn: { type: String, trim: true },
+    sellingPrice: { type: Number, default: 0, min: 0 },
     createdByClient: { type: mongoose.Schema.Types.ObjectId, ref: "Client", required: true },
     createdByUser: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   },
