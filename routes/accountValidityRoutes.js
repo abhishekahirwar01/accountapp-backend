cc9d379a849d92b31b0fb185116b5cc43b2830c4
Finaxis002 <finaxis.ai@gmail.com>
2025-11-03 19:01:47 +0530

    route for validity note on dashboard

--- routes/accountValidityRoutes.js
+++ routes/accountValidityRoutes.js
@@ -2,11 +2,14 @@
 const express = require("express");
 const router = express.Router();
 const verifyMasterAdmin = require("../middleware/verifyMasterAdmin");
-const { setValidity, getValidity, disableValidity, updateValidityPartial} = require("../controllers/accountValidityController");
+const verifyClientOrAdmin = require("../middleware/verifyClientOrAdmin")
+const { setValidity, getValidity, disableValidity, updateValidityPartial, getMyValidity} = require("../controllers/accountValidityController");
 
 router.put("/:clientId/validity", verifyMasterAdmin, setValidity);      // create/update
+router.get("/me/validity", verifyClientOrAdmin, getMyValidity);
 router.get("/:clientId/validity", verifyMasterAdmin, getValidity);      // read
 router.patch("/:clientId/validity", verifyMasterAdmin, updateValidityPartial); 
 router.patch("/:clientId/validity/disable", verifyMasterAdmin, disableValidity);
+
 
 module.exports = router;
