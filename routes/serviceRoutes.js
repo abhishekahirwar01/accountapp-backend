79c6c6d19790b304caa130a0ae87a5706d55e2f4
finaxis002 <finaxis.ai@gmail.com>
2025-11-17 17:25:23 +0530

    fixes in template import

--- routes/serviceRoutes.js
+++ routes/serviceRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const multer = require("multer");
 
 const verifyClientOrAdmin = require("../middleware/verifyClientOrAdmin");
 const serviceController = require("../controllers/serviceController");
@@ -16,5 +17,12 @@ router.put("/:id", verifyClientOrAdmin, serviceController.updateService);
 router.delete("/:id", verifyClientOrAdmin, serviceController.deleteService);
 
 router.get("/:id", verifyClientOrAdmin, serviceController.getServiceById);
+
+router.get('/import/template',verifyClientOrAdmin, serviceController.downloadImportTemplate);
+// Set up multer storage for file handling
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+router.post('/import', verifyClientOrAdmin , upload.single('file'), serviceController.importServices);
 
 module.exports = router;
